e4ce810011c35e7b0dabd9721eb0ee69837db2dc
Bharpur Singh <bharpursingh3081@outlook.com>
2024-07-28 01:17:39 -0400

    CORS origin added for Frontend

--- src/main/java/com/example/ApplyMate/controller/JobController.java
+++ src/main/java/com/example/ApplyMate/controller/JobController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin("http://127.0.0.1:3000/")
+@CrossOrigin("*")
 @RequestMapping("/api/jobs")
 public class JobController {
 
